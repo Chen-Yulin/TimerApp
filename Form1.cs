2cd12fdce71b18986e90cde5145c893e5961e196
chenyulin <chenyulin@sjtu.edu.cn>
2023-04-30 21:32:34 +0800

    finish timmer

--- Form1.cs
+++ Form1.cs
@@ -13,10 +13,15 @@ namespace TimerApp
 {
     public partial class Form1 : Form
     {
+        public float roundLimit = 2  * 1000f;
+        public float totalLimit = 10  * 1000f;
+
         private System.Timers.Timer timer1;
         private Stopwatch stopwatch1;
         private System.Timers.Timer timer2;
         private Stopwatch stopwatch2;
+        private System.Timers.Timer timer3;
+        private Stopwatch stopwatch3;
         string FormatTime(long milliseconds)
         {
             int totalSeconds = (int)(milliseconds / 1000);
@@ -45,23 +50,43 @@ namespace TimerApp
             timer2.SynchronizingObject = this;
             stopwatch2 = new Stopwatch();
 
+            timer3 = new System.Timers.Timer();
+            timer3.Interval = 20;
+            timer3.Elapsed += Timer_Tick3;
+            timer3.AutoReset = true;
+            timer3.SynchronizingObject = this;
+            stopwatch3 = new Stopwatch();
+
             textBox1.ReadOnly = true;
             textBox2.ReadOnly = true;
         }
 
         private void Timer_Tick1(object sender, EventArgs e)
         {
-            // 更新累计时间
-
-            // 将累计时间显示在文本框中
             textBox1.Text = FormatTime(stopwatch1.ElapsedMilliseconds);
+            if (stopwatch1.IsRunning && stopwatch1.ElapsedMilliseconds >= roundLimit)
+            {
+                stopwatch1.Stop();
+                textBox1.BackColor = Color.Yellow;
+            }
         }
         private void Timer_Tick2(object sender, EventArgs e)
         {
-            // 更新累计时间
-
-            // 将累计时间显示在文本框中
             textBox2.Text = FormatTime(stopwatch2.ElapsedMilliseconds);
+            if (stopwatch2.IsRunning && stopwatch2.ElapsedMilliseconds >= roundLimit)
+            {
+                stopwatch2.Stop();
+                textBox2.BackColor = Color.Yellow;
+            }
+        }
+        private void Timer_Tick3(object sender, EventArgs e)
+        {
+            textBox3.Text = FormatTime(stopwatch3.ElapsedMilliseconds);
+            if (stopwatch3.IsRunning && stopwatch3.ElapsedMilliseconds >= totalLimit)
+            {
+                stopwatch3.Stop();
+                textBox3.BackColor = Color.Red;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -77,6 +102,8 @@ namespace TimerApp
         {
             timer1.Start();
             stopwatch1.Start();
+            timer3.Start();
+            stopwatch3.Start();
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -126,5 +153,26 @@ namespace TimerApp
         {
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            stopwatch1.Reset();
+            stopwatch2.Reset();
+            stopwatch3.Reset();
+            stopwatch1.Stop();
+            stopwatch2.Stop();
+            stopwatch3.Stop();
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox1.BackColor = Color.White;
+            textBox2.BackColor = Color.White;
+            textBox3.BackColor = Color.White;
+        }
+
     }
 }
